a9c60aed2fbd6ea6feb066470d2ed4fc32c13b93
seyidaniels <seyidaniels@gmail.com>
2019-02-02 10:48:47 +0100

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -1,17 +1,4 @@
 <?php
 return [
 
-        'driver' => 'smtp',
-
-        'host' => 'smtp.sendgrid.net',
-
-        'port' => 465,
-
-            'encryption' => 'ssl',
-
-        'username' => 'seyidaniels1',
-
-        'password' => 'iyanuoladele@0963',
-
-        'sendmail' => '/usr/sbin/sendmail -bs'
 ];
